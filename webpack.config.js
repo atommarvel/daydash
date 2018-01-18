a820b5a113bebad74e1386f6fa3609360a8f005f
Atom Raiff <atom.raiff@gmail.com>
2018-01-17 23:23:22 -0800

    working babel/webpack filewatcher

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const mod = {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['react']
+                }
+            }
+        }
+    ]
+};
+
+module.exports = {
+    module: mod,
+    entry: './index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
