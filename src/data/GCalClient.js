890b86d8626ee282f4a655574d5be3a783e77d12
Atom Raiff <atom.raiff@gmail.com>
2018-01-18 19:21:07 -0800

    api docs

--- src/data/GCalClient.js
+++ src/data/GCalClient.js
@@ -1,4 +1,5 @@
 // In charge of interacting with GCal APIs
+// https://developers.google.com/google-apps/calendar/
 
 class GCalClient {
 
--- src/data/TodoistClient.js
+++ src/data/TodoistClient.js
@@ -1,4 +1,5 @@
 // In charge of interacting with Todoist APIs
+// https://developer.todoist.com/
 
 class TodoistClient {
 
