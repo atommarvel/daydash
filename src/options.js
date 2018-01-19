31ecc8783b1544ada61067b5d08e847856bcbb2e
Atom Raiff <atom.raiff@gmail.com>
2018-01-18 19:03:09 -0800

    init options

---
+++ src/options.js
@@ -0,0 +1,27 @@
+// Saves options to chrome.storage.sync.
+function save_options() {
+    var todoKey = document.getElementById('todoKey').value;
+    chrome.storage.sync.set({
+        todoKey: todoKey
+    }, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+function restore_options() {
+    // Use default value empty string
+    chrome.storage.sync.get({
+        todoKey: ""
+    }, function(items) {
+        document.getElementById('todoKey').value = items.todoKey;
+    });
+}
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
